2502316fa3d2fab8218a38c8405704b1345b4a07
RebekkaOskarsen <111863454+RebekkaOskarsen@users.noreply.github.com>
2024-11-12 14:25:45 +0100

    Archivist walk and look around

---
+++ ArchiveMystery/Source/ArchiveMystery/Public/Character/Archivist.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "Archivist.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class ARCHIVEMYSTERY_API AArchivist : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AArchivist();
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* CharacterMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* MoveAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* LookAction;
+
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+private:
+	// Camera properties
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArm;
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* ViewCamera;
+
+};
