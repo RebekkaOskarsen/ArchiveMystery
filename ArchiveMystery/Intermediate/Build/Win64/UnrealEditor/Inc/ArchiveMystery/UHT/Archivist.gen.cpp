68dd78ff4572576b9d36a81c983523199f4b73a1
LurvenArt <minaskagseth@gmail.com>
2025-01-30 22:32:32 +0100

    Rebuilding building, added new 3D Assets, Created landscape

---
+++ ArchiveMystery/Intermediate/Build/Win64/UnrealEditor/Inc/ArchiveMystery/UHT/Archivist.gen.cpp
@@ -0,0 +1,215 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "ArchiveMystery/Public/Character/Archivist.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeArchivist() {}
+
+// Begin Cross Module References
+ARCHIVEMYSTERY_API UClass* Z_Construct_UClass_AArchivist();
+ARCHIVEMYSTERY_API UClass* Z_Construct_UClass_AArchivist_NoRegister();
+ARCHIVEMYSTERY_API UClass* Z_Construct_UClass_AItems_NoRegister();
+ARCHIVEMYSTERY_API UEnum* Z_Construct_UEnum_ArchiveMystery_ECharacterState();
+ENGINE_API UClass* Z_Construct_UClass_ACharacter();
+ENGINE_API UClass* Z_Construct_UClass_UCameraComponent_NoRegister();
+ENGINE_API UClass* Z_Construct_UClass_USpringArmComponent_NoRegister();
+ENHANCEDINPUT_API UClass* Z_Construct_UClass_UInputAction_NoRegister();
+ENHANCEDINPUT_API UClass* Z_Construct_UClass_UInputMappingContext_NoRegister();
+UPackage* Z_Construct_UPackage__Script_ArchiveMystery();
+// End Cross Module References
+
+// Begin Enum ECharacterState
+static FEnumRegistrationInfo Z_Registration_Info_UEnum_ECharacterState;
+static UEnum* ECharacterState_StaticEnum()
+{
+	if (!Z_Registration_Info_UEnum_ECharacterState.OuterSingleton)
+	{
+		Z_Registration_Info_UEnum_ECharacterState.OuterSingleton = GetStaticEnum(Z_Construct_UEnum_ArchiveMystery_ECharacterState, (UObject*)Z_Construct_UPackage__Script_ArchiveMystery(), TEXT("ECharacterState"));
+	}
+	return Z_Registration_Info_UEnum_ECharacterState.OuterSingleton;
+}
+template<> ARCHIVEMYSTERY_API UEnum* StaticEnum<ECharacterState>()
+{
+	return ECharacterState_StaticEnum();
+}
+struct Z_Construct_UEnum_ArchiveMystery_ECharacterState_Statics
+{
+#if WITH_METADATA
+	static constexpr UECodeGen_Private::FMetaDataPairParam Enum_MetaDataParams[] = {
+		{ "BlueprintType", "true" },
+		{ "ECS_EquippedOneHandedBox.DisplayName", "Equipped One-Handed Box" },
+		{ "ECS_EquippedOneHandedBox.Name", "ECharacterState::ECS_EquippedOneHandedBox" },
+		{ "ECS_Unequipped.DisplayName", "Unequipped" },
+		{ "ECS_Unequipped.Name", "ECharacterState::ECS_Unequipped" },
+		{ "ModuleRelativePath", "Public/Character/Archivist.h" },
+	};
+#endif // WITH_METADATA
+	static constexpr UECodeGen_Private::FEnumeratorParam Enumerators[] = {
+		{ "ECharacterState::ECS_Unequipped", (int64)ECharacterState::ECS_Unequipped },
+		{ "ECharacterState::ECS_EquippedOneHandedBox", (int64)ECharacterState::ECS_EquippedOneHandedBox },
+	};
+	static const UECodeGen_Private::FEnumParams EnumParams;
+};
+const UECodeGen_Private::FEnumParams Z_Construct_UEnum_ArchiveMystery_ECharacterState_Statics::EnumParams = {
+	(UObject*(*)())Z_Construct_UPackage__Script_ArchiveMystery,
+	nullptr,
+	"ECharacterState",
+	"ECharacterState",
+	Z_Construct_UEnum_ArchiveMystery_ECharacterState_Statics::Enumerators,
+	RF_Public|RF_Transient|RF_MarkAsNative,
+	UE_ARRAY_COUNT(Z_Construct_UEnum_ArchiveMystery_ECharacterState_Statics::Enumerators),
+	EEnumFlags::None,
+	(uint8)UEnum::ECppForm::EnumClass,
+	METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UEnum_ArchiveMystery_ECharacterState_Statics::Enum_MetaDataParams), Z_Construct_UEnum_ArchiveMystery_ECharacterState_Statics::Enum_MetaDataParams)
+};
+UEnum* Z_Construct_UEnum_ArchiveMystery_ECharacterState()
+{
+	if (!Z_Registration_Info_UEnum_ECharacterState.InnerSingleton)
+	{
+		UECodeGen_Private::ConstructUEnum(Z_Registration_Info_UEnum_ECharacterState.InnerSingleton, Z_Construct_UEnum_ArchiveMystery_ECharacterState_Statics::EnumParams);
+	}
+	return Z_Registration_Info_UEnum_ECharacterState.InnerSingleton;
+}
+// End Enum ECharacterState
+
+// Begin Class AArchivist
+void AArchivist::StaticRegisterNativesAArchivist()
+{
+}
+IMPLEMENT_CLASS_NO_AUTO_REGISTRATION(AArchivist);
+UClass* Z_Construct_UClass_AArchivist_NoRegister()
+{
+	return AArchivist::StaticClass();
+}
+struct Z_Construct_UClass_AArchivist_Statics
+{
+#if WITH_METADATA
+	static constexpr UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+		{ "HideCategories", "Navigation" },
+		{ "IncludePath", "Character/Archivist.h" },
+		{ "ModuleRelativePath", "Public/Character/Archivist.h" },
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_CharacterMappingContext_MetaData[] = {
+		{ "Category", "Input" },
+		{ "ModuleRelativePath", "Public/Character/Archivist.h" },
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_MoveAction_MetaData[] = {
+		{ "Category", "Input" },
+		{ "ModuleRelativePath", "Public/Character/Archivist.h" },
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_LookAction_MetaData[] = {
+		{ "Category", "Input" },
+		{ "ModuleRelativePath", "Public/Character/Archivist.h" },
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_PickUpAction_MetaData[] = {
+		{ "Category", "Input" },
+		{ "ModuleRelativePath", "Public/Character/Archivist.h" },
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_SpringArm_MetaData[] = {
+		{ "Category", "Archivist" },
+#if !UE_BUILD_SHIPPING
+		{ "Comment", "// Camera properties\n" },
+#endif
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "Public/Character/Archivist.h" },
+#if !UE_BUILD_SHIPPING
+		{ "ToolTip", "Camera properties" },
+#endif
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_ViewCamera_MetaData[] = {
+		{ "Category", "Archivist" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "Public/Character/Archivist.h" },
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_OverlappingItems_MetaData[] = {
+		{ "Category", "Archivist" },
+		{ "ModuleRelativePath", "Public/Character/Archivist.h" },
+	};
+#endif // WITH_METADATA
+	static const UECodeGen_Private::FObjectPropertyParams NewProp_CharacterMappingContext;
+	static const UECodeGen_Private::FObjectPropertyParams NewProp_MoveAction;
+	static const UECodeGen_Private::FObjectPropertyParams NewProp_LookAction;
+	static const UECodeGen_Private::FObjectPropertyParams NewProp_PickUpAction;
+	static const UECodeGen_Private::FObjectPropertyParams NewProp_SpringArm;
+	static const UECodeGen_Private::FObjectPropertyParams NewProp_ViewCamera;
+	static const UECodeGen_Private::FObjectPropertyParams NewProp_OverlappingItems;
+	static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
+	static UObject* (*const DependentSingletons[])();
+	static constexpr FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AArchivist>::IsAbstract,
+	};
+	static const UECodeGen_Private::FClassParams ClassParams;
+};
+const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_AArchivist_Statics::NewProp_CharacterMappingContext = { "CharacterMappingContext", nullptr, (EPropertyFlags)0x0020080000000015, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AArchivist, CharacterMappingContext), Z_Construct_UClass_UInputMappingContext_NoRegister, METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_CharacterMappingContext_MetaData), NewProp_CharacterMappingContext_MetaData) };
+const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_AArchivist_Statics::NewProp_MoveAction = { "MoveAction", nullptr, (EPropertyFlags)0x0020080000000015, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AArchivist, MoveAction), Z_Construct_UClass_UInputAction_NoRegister, METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_MoveAction_MetaData), NewProp_MoveAction_MetaData) };
+const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_AArchivist_Statics::NewProp_LookAction = { "LookAction", nullptr, (EPropertyFlags)0x0020080000000015, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AArchivist, LookAction), Z_Construct_UClass_UInputAction_NoRegister, METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_LookAction_MetaData), NewProp_LookAction_MetaData) };
+const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_AArchivist_Statics::NewProp_PickUpAction = { "PickUpAction", nullptr, (EPropertyFlags)0x0020080000000015, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AArchivist, PickUpAction), Z_Construct_UClass_UInputAction_NoRegister, METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_PickUpAction_MetaData), NewProp_PickUpAction_MetaData) };
+const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_AArchivist_Statics::NewProp_SpringArm = { "SpringArm", nullptr, (EPropertyFlags)0x00400000000a0009, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AArchivist, SpringArm), Z_Construct_UClass_USpringArmComponent_NoRegister, METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_SpringArm_MetaData), NewProp_SpringArm_MetaData) };
+const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_AArchivist_Statics::NewProp_ViewCamera = { "ViewCamera", nullptr, (EPropertyFlags)0x00400000000a0009, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AArchivist, ViewCamera), Z_Construct_UClass_UCameraComponent_NoRegister, METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_ViewCamera_MetaData), NewProp_ViewCamera_MetaData) };
+const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_AArchivist_Statics::NewProp_OverlappingItems = { "OverlappingItems", nullptr, (EPropertyFlags)0x0040000000020801, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(AArchivist, OverlappingItems), Z_Construct_UClass_AItems_NoRegister, METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_OverlappingItems_MetaData), NewProp_OverlappingItems_MetaData) };
+const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_AArchivist_Statics::PropPointers[] = {
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AArchivist_Statics::NewProp_CharacterMappingContext,
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AArchivist_Statics::NewProp_MoveAction,
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AArchivist_Statics::NewProp_LookAction,
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AArchivist_Statics::NewProp_PickUpAction,
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AArchivist_Statics::NewProp_SpringArm,
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AArchivist_Statics::NewProp_ViewCamera,
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_AArchivist_Statics::NewProp_OverlappingItems,
+};
+static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_AArchivist_Statics::PropPointers) < 2048);
+UObject* (*const Z_Construct_UClass_AArchivist_Statics::DependentSingletons[])() = {
+	(UObject* (*)())Z_Construct_UClass_ACharacter,
+	(UObject* (*)())Z_Construct_UPackage__Script_ArchiveMystery,
+};
+static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_AArchivist_Statics::DependentSingletons) < 16);
+const UECodeGen_Private::FClassParams Z_Construct_UClass_AArchivist_Statics::ClassParams = {
+	&AArchivist::StaticClass,
+	"Game",
+	&StaticCppClassTypeInfo,
+	DependentSingletons,
+	nullptr,
+	Z_Construct_UClass_AArchivist_Statics::PropPointers,
+	nullptr,
+	UE_ARRAY_COUNT(DependentSingletons),
+	0,
+	UE_ARRAY_COUNT(Z_Construct_UClass_AArchivist_Statics::PropPointers),
+	0,
+	0x009000A4u,
+	METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_AArchivist_Statics::Class_MetaDataParams), Z_Construct_UClass_AArchivist_Statics::Class_MetaDataParams)
+};
+UClass* Z_Construct_UClass_AArchivist()
+{
+	if (!Z_Registration_Info_UClass_AArchivist.OuterSingleton)
+	{
+		UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_AArchivist.OuterSingleton, Z_Construct_UClass_AArchivist_Statics::ClassParams);
+	}
+	return Z_Registration_Info_UClass_AArchivist.OuterSingleton;
+}
+template<> ARCHIVEMYSTERY_API UClass* StaticClass<AArchivist>()
+{
+	return AArchivist::StaticClass();
+}
+DEFINE_VTABLE_PTR_HELPER_CTOR(AArchivist);
+AArchivist::~AArchivist() {}
+// End Class AArchivist
+
+// Begin Registration
+struct Z_CompiledInDeferFile_FID_Users_minas_OneDrive_Dokumenter_Personal_Projects_NGA_2025_ArchiveMystery_ArchiveMystery_Source_ArchiveMystery_Public_Character_Archivist_h_Statics
+{
+	static constexpr FEnumRegisterCompiledInInfo EnumInfo[] = {
+		{ ECharacterState_StaticEnum, TEXT("ECharacterState"), &Z_Registration_Info_UEnum_ECharacterState, CONSTRUCT_RELOAD_VERSION_INFO(FEnumReloadVersionInfo, 328310748U) },
+	};
+	static constexpr FClassRegisterCompiledInInfo ClassInfo[] = {
+		{ Z_Construct_UClass_AArchivist, AArchivist::StaticClass, TEXT("AArchivist"), &Z_Registration_Info_UClass_AArchivist, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AArchivist), 4114187410U) },
+	};
+};
+static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_minas_OneDrive_Dokumenter_Personal_Projects_NGA_2025_ArchiveMystery_ArchiveMystery_Source_ArchiveMystery_Public_Character_Archivist_h_2422892113(TEXT("/Script/ArchiveMystery"),
+	Z_CompiledInDeferFile_FID_Users_minas_OneDrive_Dokumenter_Personal_Projects_NGA_2025_ArchiveMystery_ArchiveMystery_Source_ArchiveMystery_Public_Character_Archivist_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_minas_OneDrive_Dokumenter_Personal_Projects_NGA_2025_ArchiveMystery_ArchiveMystery_Source_ArchiveMystery_Public_Character_Archivist_h_Statics::ClassInfo),
+	nullptr, 0,
+	Z_CompiledInDeferFile_FID_Users_minas_OneDrive_Dokumenter_Personal_Projects_NGA_2025_ArchiveMystery_ArchiveMystery_Source_ArchiveMystery_Public_Character_Archivist_h_Statics::EnumInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_minas_OneDrive_Dokumenter_Personal_Projects_NGA_2025_ArchiveMystery_ArchiveMystery_Source_ArchiveMystery_Public_Character_Archivist_h_Statics::EnumInfo));
+// End Registration
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
