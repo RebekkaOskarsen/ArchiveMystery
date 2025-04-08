1b0013eebf9ef369ea8f107ebb0bbe3e49ca983b
LurvenArt <minaskagseth@gmail.com>
2025-04-08 11:54:30 +0200

    Main menu works :D

--- ArchiveMystery/Intermediate/Build/Win64/UnrealEditor/Inc/ArchiveMystery/UHT/ArchiveMystery.init.gen.cpp
+++ ArchiveMystery/Intermediate/Build/Win64/UnrealEditor/Inc/ArchiveMystery/UHT/ArchiveMystery.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeArchiveMystery_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xA7C14560,
+				0x49601CAA,
 				0xF6433224,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeArchiveMystery_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_ArchiveMystery.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ArchiveMystery(Z_Construct_UPackage__Script_ArchiveMystery, TEXT("/Script/ArchiveMystery"), Z_Registration_Info_UPackage__Script_ArchiveMystery, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xA7C14560, 0xF6433224));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ArchiveMystery(Z_Construct_UPackage__Script_ArchiveMystery, TEXT("/Script/ArchiveMystery"), Z_Registration_Info_UPackage__Script_ArchiveMystery, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x49601CAA, 0xF6433224));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- ArchiveMystery/Intermediate/Build/Win64/x64/ArchiveMysteryEditor/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.h
+++ ArchiveMystery/Intermediate/Build/Win64/x64/ArchiveMysteryEditor/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
-#include "C:/ArchiveMystery/ArchiveMystery/ArchiveMystery/Intermediate/Build/Win64/x64/ArchiveMysteryEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.RTTI.Cpp20.h"
+#include "C:/Users/minas/OneDrive/Dokumenter/Personal Projects/NGA 2025/ArchiveMystery/ArchiveMystery/Intermediate/Build/Win64/x64/ArchiveMysteryEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.RTTI.Cpp20.h"
 #include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
