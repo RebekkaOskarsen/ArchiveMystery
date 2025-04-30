339f6807fd26f97b85d86428be99478c011164e2
CathrineSageng <111863594+CathrineSageng@users.noreply.github.com>
2025-04-30 14:59:40 +0200

    some bugs fixing

---
+++ ArchiveMystery/Intermediate/Build/Win64/UnrealEditor/Inc/ArchiveMystery/UHT/PaintingInfo.generated.h
@@ -0,0 +1,28 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Character/PaintingInfo.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ARCHIVEMYSTERY_PaintingInfo_generated_h
+#error "PaintingInfo.generated.h already included, missing '#pragma once' in PaintingInfo.h"
+#endif
+#define ARCHIVEMYSTERY_PaintingInfo_generated_h
+
+#define FID_ArchiveMystery_ArchiveMystery_ArchiveMystery_Source_ArchiveMystery_Public_Character_PaintingInfo_h_17_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FPaintingInfo_Statics; \
+	ARCHIVEMYSTERY_API static class UScriptStruct* StaticStruct();
+
+
+template<> ARCHIVEMYSTERY_API UScriptStruct* StaticStruct<struct FPaintingInfo>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_ArchiveMystery_ArchiveMystery_ArchiveMystery_Source_ArchiveMystery_Public_Character_PaintingInfo_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
