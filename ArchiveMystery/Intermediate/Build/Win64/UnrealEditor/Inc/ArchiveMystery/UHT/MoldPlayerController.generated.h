ed6edab7aa42a97d15fdeb3795b8a8ff7a44ff4f
henrikvestad <vestadhenrik@gmail.com>
2025-05-03 23:04:11 +0200

    fixed customization buttons, trash asset and applied some textures

---
+++ ArchiveMystery/Intermediate/Build/Win64/UnrealEditor/Inc/ArchiveMystery/UHT/MoldPlayerController.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Mold/MoldPlayerController.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ARCHIVEMYSTERY_MoldPlayerController_generated_h
+#error "MoldPlayerController.generated.h already included, missing '#pragma once' in MoldPlayerController.h"
+#endif
+#define ARCHIVEMYSTERY_MoldPlayerController_generated_h
+
+#define FID_Users_henri_OneDrive___Universitetet_i_Innlandet_Skrivebord_saturday_ArchiveMystery_Source_ArchiveMystery_Public_Mold_MoldPlayerController_h_17_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMoldPlayerController(); \
+	friend struct Z_Construct_UClass_AMoldPlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AMoldPlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ArchiveMystery"), NO_API) \
+	DECLARE_SERIALIZER(AMoldPlayerController)
+
+
+#define FID_Users_henri_OneDrive___Universitetet_i_Innlandet_Skrivebord_saturday_ArchiveMystery_Source_ArchiveMystery_Public_Mold_MoldPlayerController_h_17_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	AMoldPlayerController(AMoldPlayerController&&); \
+	AMoldPlayerController(const AMoldPlayerController&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMoldPlayerController); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMoldPlayerController); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AMoldPlayerController) \
+	NO_API virtual ~AMoldPlayerController();
+
+
+#define FID_Users_henri_OneDrive___Universitetet_i_Innlandet_Skrivebord_saturday_ArchiveMystery_Source_ArchiveMystery_Public_Mold_MoldPlayerController_h_14_PROLOG
+#define FID_Users_henri_OneDrive___Universitetet_i_Innlandet_Skrivebord_saturday_ArchiveMystery_Source_ArchiveMystery_Public_Mold_MoldPlayerController_h_17_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_henri_OneDrive___Universitetet_i_Innlandet_Skrivebord_saturday_ArchiveMystery_Source_ArchiveMystery_Public_Mold_MoldPlayerController_h_17_INCLASS_NO_PURE_DECLS \
+	FID_Users_henri_OneDrive___Universitetet_i_Innlandet_Skrivebord_saturday_ArchiveMystery_Source_ArchiveMystery_Public_Mold_MoldPlayerController_h_17_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ARCHIVEMYSTERY_API UClass* StaticClass<class AMoldPlayerController>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_henri_OneDrive___Universitetet_i_Innlandet_Skrivebord_saturday_ArchiveMystery_Source_ArchiveMystery_Public_Mold_MoldPlayerController_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
