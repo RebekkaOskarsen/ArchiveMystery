620b05403531e3d70c383ff47090b2aa90233f39
LurvenArt <minaskagseth@gmail.com>
2025-07-30 10:25:11 +0200

    Soda can sfx, coat and hanger, localization

--- ArchiveMystery/Intermediate/Build/Win64/x64/ArchiveMysteryEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.InclOrderUnreal5_3.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "C:/Users/henri/Downloads/tirsdag/ArchiveMystery/Intermediate/Build/Win64/x64/ArchiveMysteryEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Project.ValApi.Cpp20.InclOrderUnreal5_3.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
