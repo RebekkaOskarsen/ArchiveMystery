29e7c3c8d38560f4eeb5d112e175c862b29dac0a
henrikvestad <vestadhenrik@gmail.com>
2025-07-30 12:24:07 +0200

    widgets rename, outline

---
+++ ArchiveMystery/Intermediate/Build/Win64/UnrealEditor/Inc/ArchiveMystery/UHT/DatabaseWidget.generated.h
@@ -0,0 +1,67 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Database/DatabaseWidget.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ARCHIVEMYSTERY_DatabaseWidget_generated_h
+#error "DatabaseWidget.generated.h already included, missing '#pragma once' in DatabaseWidget.h"
+#endif
+#define ARCHIVEMYSTERY_DatabaseWidget_generated_h
+
+#define FID_Users_henri_Downloads_onsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_DatabaseWidget_h_21_RPC_WRAPPERS_NO_PURE_DECLS \
+	DECLARE_FUNCTION(execOnOpenInfoClicked); \
+	DECLARE_FUNCTION(execOnAndTextCommitted); \
+	DECLARE_FUNCTION(execOnLikeTextCommitted); \
+	DECLARE_FUNCTION(execOnWhereTextCommitted); \
+	DECLARE_FUNCTION(execOnFromTextCommitted); \
+	DECLARE_FUNCTION(execOnSelectTextCommitted); \
+	DECLARE_FUNCTION(execOnSubmitClicked);
+
+
+#define FID_Users_henri_Downloads_onsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_DatabaseWidget_h_21_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUDatabaseWidget(); \
+	friend struct Z_Construct_UClass_UDatabaseWidget_Statics; \
+public: \
+	DECLARE_CLASS(UDatabaseWidget, UUserWidget, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/ArchiveMystery"), NO_API) \
+	DECLARE_SERIALIZER(UDatabaseWidget)
+
+
+#define FID_Users_henri_Downloads_onsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_DatabaseWidget_h_21_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UDatabaseWidget(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UDatabaseWidget(UDatabaseWidget&&); \
+	UDatabaseWidget(const UDatabaseWidget&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UDatabaseWidget); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UDatabaseWidget); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UDatabaseWidget) \
+	NO_API virtual ~UDatabaseWidget();
+
+
+#define FID_Users_henri_Downloads_onsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_DatabaseWidget_h_18_PROLOG
+#define FID_Users_henri_Downloads_onsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_DatabaseWidget_h_21_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_henri_Downloads_onsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_DatabaseWidget_h_21_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_henri_Downloads_onsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_DatabaseWidget_h_21_INCLASS_NO_PURE_DECLS \
+	FID_Users_henri_Downloads_onsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_DatabaseWidget_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ARCHIVEMYSTERY_API UClass* StaticClass<class UDatabaseWidget>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_henri_Downloads_onsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_DatabaseWidget_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
