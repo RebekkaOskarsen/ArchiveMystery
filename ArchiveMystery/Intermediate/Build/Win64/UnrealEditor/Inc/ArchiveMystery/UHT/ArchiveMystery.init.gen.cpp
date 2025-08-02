27ca19ce5fbea33f13f0cf6218fc067cad52421a
henrikvestad <vestadhenrik@gmail.com>
2025-08-02 02:04:15 +0200

    bicycle + stand

---
+++ ArchiveMystery/Intermediate/Build/Win64/UnrealEditor/Inc/ArchiveMystery/UHT/ArchiveMystery.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeArchiveMystery_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_ArchiveMystery;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_ArchiveMystery()
+	{
+		if (!Z_Registration_Info_UPackage__Script_ArchiveMystery.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/ArchiveMystery",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x0B9F14F5,
+				0xB93EB6DA,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_ArchiveMystery.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_ArchiveMystery.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ArchiveMystery(Z_Construct_UPackage__Script_ArchiveMystery, TEXT("/Script/ArchiveMystery"), Z_Registration_Info_UPackage__Script_ArchiveMystery, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x0B9F14F5, 0xB93EB6DA));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
