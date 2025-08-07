ce4e5bc6d8a17a33ca50b2224eb918722f67f2fb
LurvenArt <minaskagseth@gmail.com>
2025-08-07 16:10:53 +0200

    Score UI for minigames, R Key, garage word removed

--- ArchiveMystery/Intermediate/Build/Win64/x64/ArchiveMysteryEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/ArchiveMystery/ArchiveMystery/ArchiveMystery/Intermediate/Build/Win64/x64/ArchiveMysteryEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
