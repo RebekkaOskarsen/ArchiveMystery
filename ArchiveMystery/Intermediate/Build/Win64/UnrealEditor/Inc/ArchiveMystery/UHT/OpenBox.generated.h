1d07b0ad7e67bd2e8222bb95ea5eb765cdb409f2
henrikvestad <vestadhenrik@gmail.com>
2025-08-21 08:30:03 +0200

    sign textures, glass wall, quest marker page

---
+++ ArchiveMystery/Intermediate/Build/Win64/UnrealEditor/Inc/ArchiveMystery/UHT/OpenBox.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Items/Box/OpenBox.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ARCHIVEMYSTERY_OpenBox_generated_h
+#error "OpenBox.generated.h already included, missing '#pragma once' in OpenBox.h"
+#endif
+#define ARCHIVEMYSTERY_OpenBox_generated_h
+
+#define FID_Users_henri_Downloads_torsdag_ArchiveMystery_Source_ArchiveMystery_Public_Items_Box_OpenBox_h_14_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAOpenBox(); \
+	friend struct Z_Construct_UClass_AOpenBox_Statics; \
+public: \
+	DECLARE_CLASS(AOpenBox, AItems, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ArchiveMystery"), NO_API) \
+	DECLARE_SERIALIZER(AOpenBox)
+
+
+#define FID_Users_henri_Downloads_torsdag_ArchiveMystery_Source_ArchiveMystery_Public_Items_Box_OpenBox_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	AOpenBox(AOpenBox&&); \
+	AOpenBox(const AOpenBox&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AOpenBox); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AOpenBox); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AOpenBox) \
+	NO_API virtual ~AOpenBox();
+
+
+#define FID_Users_henri_Downloads_torsdag_ArchiveMystery_Source_ArchiveMystery_Public_Items_Box_OpenBox_h_11_PROLOG
+#define FID_Users_henri_Downloads_torsdag_ArchiveMystery_Source_ArchiveMystery_Public_Items_Box_OpenBox_h_14_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_henri_Downloads_torsdag_ArchiveMystery_Source_ArchiveMystery_Public_Items_Box_OpenBox_h_14_INCLASS_NO_PURE_DECLS \
+	FID_Users_henri_Downloads_torsdag_ArchiveMystery_Source_ArchiveMystery_Public_Items_Box_OpenBox_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ARCHIVEMYSTERY_API UClass* StaticClass<class AOpenBox>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_henri_Downloads_torsdag_ArchiveMystery_Source_ArchiveMystery_Public_Items_Box_OpenBox_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ ArchiveMystery/Intermediate/Build/Win64/UnrealEditor/Inc/ArchiveMystery/UHT/StartPageWidget.generated.h
@@ -0,0 +1,56 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Database/StartPageWidget.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ARCHIVEMYSTERY_StartPageWidget_generated_h
+#error "StartPageWidget.generated.h already included, missing '#pragma once' in StartPageWidget.h"
+#endif
+#define ARCHIVEMYSTERY_StartPageWidget_generated_h
+
+#define FID_Users_henri_Downloads_torsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_StartPageWidget_h_18_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUStartPageWidget(); \
+	friend struct Z_Construct_UClass_UStartPageWidget_Statics; \
+public: \
+	DECLARE_CLASS(UStartPageWidget, UUserWidget, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/ArchiveMystery"), NO_API) \
+	DECLARE_SERIALIZER(UStartPageWidget)
+
+
+#define FID_Users_henri_Downloads_torsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_StartPageWidget_h_18_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UStartPageWidget(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UStartPageWidget(UStartPageWidget&&); \
+	UStartPageWidget(const UStartPageWidget&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UStartPageWidget); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UStartPageWidget); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UStartPageWidget) \
+	NO_API virtual ~UStartPageWidget();
+
+
+#define FID_Users_henri_Downloads_torsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_StartPageWidget_h_15_PROLOG
+#define FID_Users_henri_Downloads_torsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_StartPageWidget_h_18_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_henri_Downloads_torsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_StartPageWidget_h_18_INCLASS_NO_PURE_DECLS \
+	FID_Users_henri_Downloads_torsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_StartPageWidget_h_18_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ARCHIVEMYSTERY_API UClass* StaticClass<class UStartPageWidget>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_henri_Downloads_torsdag_ArchiveMystery_Source_ArchiveMystery_Public_Database_StartPageWidget_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
